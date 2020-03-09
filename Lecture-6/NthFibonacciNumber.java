df686b010b6e6208c3481091642912facec7dddc
Daksh-Dummy <daksh.dummy@gmail.com>
2020-03-09 14:05:40 +0530

    added program

--- Lecture-6/NthFibonacciNumber.java
+++ Lecture-6/NthFibonacciNumber.java
@@ -9,6 +9,11 @@ public class Solution {
         
 		int a = 1, b = 1, c = 0;
         
+        if(n == 1 || n == 2){
+            System.out.println("1");
+            return;
+        }
+        
 		for(int i = 0; i < n-2; i++) {
 			c = a + b;
 			a = b;
